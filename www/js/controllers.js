d1a91891556568e88d938dd2c86cee3f0235611a
konrad <kocasp@gmail.com>
2015-02-10 22:14:38 +0100

    dev version

---
+++ www/js/controllers.js
@@ -0,0 +1,31 @@
+angular.module('starter.controllers', [])
+
+.controller('OverviewCtrl', function($scope, People) {
+  $scope.people = People.all();
+})
+
+.controller('PeopleCtrl', function($scope, People) {
+  $scope.people = People.all();
+  $scope.remove = function(person) {
+    People.remove(person);
+  }
+})
+
+.controller('PersonDetailCtrl', function($scope, $stateParams, People) {
+  $scope.person = People.get($stateParams.personId);
+  console.log($stateParams.personId)
+})
+
+.controller('FriendsCtrl', function($scope, Friends) {
+  $scope.friends = Friends.all();
+})
+
+.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+  $scope.friend = Friends.get($stateParams.friendId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
